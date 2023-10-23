0e9145dda2972e0418dceae15595d53eb6a86841
shaiadul <devshaiadul69@gmail.com>
2023-10-23 21:05:04 +0600

    posts adding

--- model/postModels.js
+++ model/postModels.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     title: {type: String, required: true},
     content: {type: String, required: true},
+    photo: {type: String, default: "no photo"},
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
     likesBy: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,3 +1,103 @@
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - title
+ *         - content
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: Title of the post
+ *         content:
+ *           type: string
+ *           description: Content of the post
+ */
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: Posts
+ *   description: Post management and retrieval
+ */
+
+/**
+ * @swagger
+ * /api/posts/create:
+ *   post:
+ *     tags:
+ *       - Posts
+ *     description: Create a new post
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         description: Post object
+ *         required: true
+ *         schema:
+ *           $ref: '#/components/schemas/Post'  # Reference the Post schema
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+
+
+/**
+ * @swagger
+ * /api/posts/like/{postId}/{userId}:
+ *   post:
+ *     tags:
+ *       - Posts
+ *     description: Like a post
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         required: true
+ *         type: string
+ *         description: ID of the post to like
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         type: string
+ *         description: ID of the user liking the post
+ *     responses:
+ *       200:
+ *         description: Successfully liked the post
+ *       400:
+ *         description: Error message if the post or user doesn't exist or if already liked
+ */
+
+
+/**
+ * @swagger
+ * /api/posts/dislike/{postId}/{userId}:
+ *   post:
+ *     tags:
+ *       - Posts
+ *     description: Dislike a post
+ *     parameters:
+ *       - name: postId
+ *         in: path
+ *         required: true
+ *         type: string
+ *         description: ID of the post to dislike
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         type: string
+ *         description: ID of the user disliking the post
+ *     responses:
+ *       200:
+ *         description: Successfully disliked the post
+ *       400:
+ *         description: Error message if the post or user doesn't exist or if already disliked
+ */
+
 import  express  from "express";
 import { createPost, dislike, like } from "../controller/postController.js";
 
